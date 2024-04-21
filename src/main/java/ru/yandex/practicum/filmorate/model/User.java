b1e0d3500e1371169afe8fa37d9bf6ae1c04b5d6
17239220 <Dmitriev.I.Alek@sberbank.ru>
2024-04-21 17:41:41 +0300

    2
    2

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -12,8 +12,7 @@ public class User {
     @Email(message = "Email is incorrect")
     @NotNull(message = "Email is required")
     private String email;
-    @NotBlank(message = "Login is required")
-    @Pattern(regexp = "\\S+", message = "Login must not contain space characters")
+    @NotNull(message = "Login is required")
     private String login;
     private String name;
     @NotNull(message = "Birthday is required")
