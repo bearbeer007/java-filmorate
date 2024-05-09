9bf5c2c2a8d8256401f13fbfbed91890d46cf92c
17239220 <Dmitriev.I.Alek@sberbank.ru>
2024-05-09 18:37:14 +0300

    style
    style

--- src/main/java/ru/yandex/practicum/filmorate/services/FilmServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/services/FilmServiceImpl.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.storage.interfaces.UserStorage;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
