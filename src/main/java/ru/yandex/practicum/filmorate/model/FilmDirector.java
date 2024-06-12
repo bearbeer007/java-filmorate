398d1915597f99d8cddbc1756a222d826e930230
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-06-12 12:14:42 +0300

    add-search v

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmDirector.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmDirector.java
@@ -1,11 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @Builder
 public class FilmDirector {
     private Long filmId;
