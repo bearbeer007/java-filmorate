378bdce515e30a376e41ba4fb3b1be916e0cbe93
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-06-12 12:16:29 +0300

    add-search v2.3

--- src/main/java/ru/yandex/practicum/filmorate/exeption/DirectorNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exeption/DirectorNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exeption;
 
-public class DirectorNotFoundException extends RuntimeException{
+public class DirectorNotFoundException extends RuntimeException {
     public DirectorNotFoundException(final String message) {
         super(message);
     }
