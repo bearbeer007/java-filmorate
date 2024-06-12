aec6d3d8e0950d1f70fc56784e69f59fbf446d94
Mikhail <mikhail.khodyrev.survai@gmail.com>
2024-06-12 13:24:35 +0300

    add-director v1.3

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmDirector.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmDirector.java
@@ -1,11 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 @Builder
 public class FilmDirector {
     private Long filmId;
